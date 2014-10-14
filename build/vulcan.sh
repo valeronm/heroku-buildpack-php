03dbd0bc8d761fccdc6eaeeaccd1ba85554e3ec2
Valeriy Mironov <valeronm@gmail.com>
2014-10-14 18:02:16 +0300

    Updated versions

--- build/vulcan.sh
+++ build/vulcan.sh
@@ -32,11 +32,11 @@ curl -L https://launchpad.net/libmemcached/1.0/1.0.16/+download/libmemcached-1.0
 echo "downloading PCRE"
 curl -L ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.34.tar.gz -o /tmp/pcre-8.34.tar.gz
 echo "downloading apr"
-curl -L ${APACHE_MIRROR_HOST}/apr/apr-1.5.0.tar.gz -o /tmp/apr-1.5.0.tar.gz
+curl -L ${APACHE_MIRROR_HOST}/apr/apr-1.5.1.tar.gz -o /tmp/apr-1.5.1.tar.gz
 echo "downloading apr-util"
-curl -L ${APACHE_MIRROR_HOST}/apr/apr-util-1.5.3.tar.gz -o /tmp/apr-util-1.5.3.tar.gz
+curl -L ${APACHE_MIRROR_HOST}/apr/apr-util-1.5.4.tar.gz -o /tmp/apr-util-1.5.4.tar.gz
 echo "downloading httpd"
-curl -L ${APACHE_MIRROR_HOST}/httpd/httpd-2.4.9.tar.gz -o /tmp/httpd-2.4.9.tar.gz
+curl -L ${APACHE_MIRROR_HOST}/httpd/httpd-2.4.10.tar.gz -o /tmp/httpd-2.4.10.tar.gz
 echo "downloading php"
 curl -L http://us.php.net/get/php-5.5.11.tar.gz/from/us2.php.net/mirror -o /tmp/php-5.5.11.tar.gz
 echo "downloading pecl-memcached"
@@ -50,13 +50,13 @@ curl -L http://zlib.net/zlib-1.2.8.tar.gz -o /tmp/zlib-1.2.8.tar.gz
 # tar -C /tmp -xzf /tmp/cyrus-sasl-2.1.25.tar.gz
 tar -C /tmp -xzf /tmp/libmemcached-1.0.16.tar.gz
 tar -C /tmp -xzf /tmp/pcre-8.34.tar.gz
-tar -C /tmp -xzf /tmp/httpd-2.4.9.tar.gz
+tar -C /tmp -xzf /tmp/httpd-2.4.10.tar.gz
 
-tar -C /tmp/httpd-2.4.9/srclib -xzf /tmp/apr-1.5.0.tar.gz
-mv /tmp/httpd-2.4.9/srclib/apr-1.5.0 /tmp/httpd-2.4.9/srclib/apr
+tar -C /tmp/httpd-2.4.10/srclib -xzf /tmp/apr-1.5.1.tar.gz
+mv /tmp/httpd-2.4.10/srclib/apr-1.5.1 /tmp/httpd-2.4.10/srclib/apr
 
-tar -C /tmp/httpd-2.4.9/srclib -xzf /tmp/apr-util-1.5.3.tar.gz
-mv /tmp/httpd-2.4.9/srclib/apr-util-1.5.3 /tmp/httpd-2.4.9/srclib/apr-util
+tar -C /tmp/httpd-2.4.10/srclib -xzf /tmp/apr-util-1.5.4.tar.gz
+mv /tmp/httpd-2.4.10/srclib/apr-util-1.5.4 /tmp/httpd-2.4.10/srclib/apr-util
 
 tar -C /tmp -xzf /tmp/php-5.5.11.tar.gz
 tar -C /tmp -xzf /tmp/memcached-2.1.0.tgz
@@ -83,14 +83,14 @@ cd /tmp/pcre-8.34
 ./configure --prefix=/app/local --enable-jit --enable-utf8
 ${MAKE} && ${MAKE} install
 
-cd /tmp/httpd-2.4.9
+cd /tmp/httpd-2.4.10
 ./configure --prefix=/app/apache --enable-rewrite --enable-so --enable-deflate --enable-expires --enable-headers --enable-proxy-fcgi --with-mpm=event --with-included-apr --with-pcre=/app/local
 ${MAKE} && ${MAKE} install
 
 cd /tmp
 git clone git://github.com/ByteInternet/libapache-mod-fastcgi.git
 cd /tmp/libapache-mod-fastcgi/
-patch -p1 < debian/patches/byte-compile-against-apache24.diff 
+patch -p1 < debian/patches/byte-compile-against-apache24.diff
 sed -e "s%/usr/local/apache2%/app/apache%" Makefile.AP2 > Makefile
 ${MAKE} && ${MAKE} install
 
@@ -131,7 +131,7 @@ ${MAKE} && ${MAKE} install
 # ./configure --prefix=/app/php --with-php-config=/app/php/bin/php-config --enable-static
 # ${MAKE} && ${MAKE} install
 
-echo '2.4.9' > /app/apache/VERSION
+echo '2.4.10' > /app/apache/VERSION
 echo '5.5.11' > /app/php/VERSION
 mkdir /tmp/build
 mkdir /tmp/build/local
